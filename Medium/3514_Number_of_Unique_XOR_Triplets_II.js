0621d94cf850e9f60bfdac2292ed63ef84a7af05
Tayyab-56 <tayyab250106@gmail.com>
2026-07-24 15:06:57 +0500

    small changes in 3514 problem.

--- Medium/3514_Number_of_Unique_XOR_Triplets_II.js
+++ Medium/3514_Number_of_Unique_XOR_Triplets_II.js
@@ -10,21 +10,21 @@ var uniqueXorTriplets = function (nums) {
     let u = 1;
     u = Math.floor(Math.log2(m)) + 1;
     u = 2 ** u;
-    const one = new Array(u).fill(false);
-    const two = new Array(u).fill(false);
-    const three = new Array(u).fill(false);
+    const one = new Uint8Array(u);
+    const two = new Uint8Array(u);
+    const three = new Uint8Array(u);
     for (const v of nums) {
-        one[v] = true;
+        one[v] = 1;
         for (let x = 0; x < u; x++) {
             if (one[x]) {
-                two[x ^ v] = true;
+                two[x ^ v] = 1;
             }
         }
     }
     for (const v of nums) {
         for (let x = 0; x < u; x++) {
             if (two[x]) {
-                three[x ^ v] = true;
+                three[x ^ v] = 1;
             }
         }
     }
